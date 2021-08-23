d0fb9caf38e47fbe0df4fb0e63e84c386a9c065c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-08-23 22:11:13 +0800

    Eureka Client Security 服务

---
+++ eureka-client-security/src/main/java/cn/com/xuxiaowei/EurekaClientSecurityApplication.java
@@ -0,0 +1,20 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ * <p>
+ * Eureka Client Security 服务
+ *
+ * @author xuxiaowei
+ */
+@SpringBootApplication
+public class EurekaClientSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientSecurityApplication.class, args);
+    }
+
+}
---
+++ eureka-client-security/src/test/java/cn/com/xuxiaowei/EurekaClientSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
